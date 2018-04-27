17897413112a18edc48fe5831296bfb447743e20
hamuPP <oppaikyouma@163.com>
2018-04-27 08:26:44 +0800

    update

--- src/components/index/rightAll.vue
+++ src/components/index/rightAll.vue
@@ -5,33 +5,65 @@
 <template>
     <div class="homepage-right-container">
         <!--<div class="homepage-right-container-inner">-->
-            <div class="homepage-right-cols cols-2">
-                <basicPanel radius>
-                    <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
-                    <p slot="panelTitleText">代办事务</p>
-                    <template slot='panelBody'>
-                        <TodoList></TodoList>
-                    </template>
-                </basicPanel>
-            </div>
-            <div class="homepage-right-cols cols-1">
-                <basicPanel radius>
-                    <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
-                    <p slot="panelTitleText">日程表</p>
-                    <template slot='panelBody'>
-                        <Calendar></Calendar>
-                    </template>
-                </basicPanel>
-            </div>
-            <div class="homepage-right-cols cols-1">
-                <basicPanel radius>
-                    <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
-                    <p slot="panelTitleText">通知公告</p>
-                    <template slot='panelBody'>
-                        <Notice></Notice>
-                    </template>
-                </basicPanel>
-            </div>
+        <div class="homepage-right-cols cols-2">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">代办事务</p>
+                <template slot='panelBody'>
+                    <TodoList></TodoList>
+                </template>
+            </basicPanel>
+        </div>
+        <div class="homepage-right-cols cols-1">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">日程表</p>
+                <template slot='panelBody'>
+                    <Calendar></Calendar>
+                </template>
+            </basicPanel>
+        </div>
+        <div class="homepage-right-cols cols-1">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">通知公告</p>
+                <template slot='panelBody'>
+                    <Notice></Notice>
+                </template>
+            </basicPanel>
+        </div>
+
+        <div class="homepage-right-cols cols-1">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">echart1</p>
+                <template slot='panelBody'>
+                    <Notice></Notice>
+                </template>
+            </basicPanel>
+        </div>
+
+        <div class="homepage-right-cols cols-1">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">echart2</p>
+                <template slot='panelBody'>
+                    <Notice></Notice>
+                </template>
+            </basicPanel>
+        </div>
+
+        <div class="homepage-right-cols cols-1">
+            <basicPanel radius>
+                <img src="../../assets/images/skin-blue/notepad.png" alt="" slot="panelLeftIconImg">
+                <p slot="panelTitleText">echart3</p>
+                <template slot='panelBody'>
+                    <Notice></Notice>
+                </template>
+            </basicPanel>
+        </div>
+
+
         <!--</div>-->
     </div>
 </template>
